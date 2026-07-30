9aff030733e7ecd75284bf2a917577c143eab998
ferranodriozola <ferranodriozola@gmail.com>
2026-07-30 14:45:54 +0200

    Update components.js

--- js/components.js
+++ js/components.js
@@ -4,8 +4,7 @@ const dataLlista = document.body ? document.body.dataset.llista : '';
 let ruta1 = '';
 let ruta2 = '';
 let estilSilabes = '';
-let botoactualitzar = ''; // abans s'assignava sense declarar: si l'id del body no era
-                          // cap dels previstos, la plantilla petava amb un ReferenceError
+let botoactualitzar = '';
 let opcionsSilabes = /*html*/`
     <option value="0">Indiferent</option>
     <option value="1">1</option>
