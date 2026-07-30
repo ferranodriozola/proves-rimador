5aeedff4da52a57d3c819a51a66a579a872f16c5
ferranodriozola <ferranodriozola@gmail.com>
2026-07-30 14:46:01 +0200

    Update script_llistes.js

--- js/script_llistes.js
+++ js/script_llistes.js
@@ -4,8 +4,6 @@ const formatarEntrada = ({ paraula, infinitiu, codi, sil, vicc, viq, diec }) =>
 function compleixFiltres({ paraula = '', codi = '', sil = '' }, numSeleccionat, comenca, incPropis, incPlurals) {
     const silabes = String(sil);
 
-    // El desplegable de components.js diu "6 o +", o sigui que l'obert és el 6, no el 5.
-    // Abans aquí el 5 era l'obert i el 6 era exacte: just al revés que a la pàgina principal.
     if (numSeleccionat === '6') {
         if (parseInt(silabes, 10) < 6) return false;
     } else if (numSeleccionat && numSeleccionat !== '0' && silabes !== numSeleccionat) {
