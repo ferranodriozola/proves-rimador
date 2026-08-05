67137165b0ee509c159cba24823f3b30d5a784f9
ferranodriozola <ferranodriozola@gmail.com>
2026-08-06 01:57:53 +0200

    Update stats.py

--- stats/stats.py
+++ stats/stats.py
@@ -17,6 +17,7 @@ df['Data'] = pd.to_datetime(df['Data'], format='%d/%m/%Y %H:%M:%S', errors='coer
 df['Dia'] = df['Data'].dt.date
 df['Usuari'] = df['Usuari'].astype(str)
 df['Paraula'] = df['Paraula'].astype(str)
+df['Codi'] = df['Codi'].astype(str)
 df['Rima'] = df['Rima'].astype(str)
 df['Tipus de rima'] = df['Tipus de rima'].astype(str)
 df['Num. síl·'] = df['Num. síl·'].astype(str)
@@ -24,23 +25,18 @@ df['Comença per'] = df['Comença per'].astype(str)
 df['Incloure NP'] = df['Incloure NP'].astype(str)
 df['Incloure pl.'] = df['Incloure pl.'].astype(str)
 
-df_net = df.drop_duplicates(subset=['Dia', 'Usuari', 'Paraula']).copy()
+# VARIABLES
+cerques_totals = df.drop_duplicates(subset=['Dia', 'Usuari', 'Paraula', 'Tipus de rima', 'Num. síl·', 'Comença per', 'Incloure NP', 'Incloure pl.']).copy()
 
-df_filtres = df.drop_duplicates(subset=['Dia', 'Usuari', 'Paraula', 'Tipus de rima', 'Num. síl·', 'Comença per', 'Incloure NP', 'Incloure pl.']).copy()
+paraules_cercades = cerques_totals.drop_duplicates(subset=['Paraula']).copy()
+paraules_cercades = paraules_cercades[paraules_cercades['Rima'] != '***'].copy()
 
+paraules_cercades_usuaris_diferents = cerques_totals.drop_duplicates(subset=['Paraula', 'Usuari']).copy()
 
-#les dues llistes
-df_typos = df_net[df_net['Rima'] == '***'].copy()
-df_rimes_totes = df_net[df_net['Rima'] != '***'].copy()
+rimes_usuaris_diferents = cerques_totals.drop_duplicates(subset=['Paraula', 'Tipus de rima', 'Usuari']).copy()
+rimes_usuaris_diferents = rimes_usuaris_diferents[['Rima', 'Tipus de rima']]
+rimes_usuaris_diferents = rimes_usuaris_diferents[rimes_usuaris_diferents['Rima'] != '***'].copy()
 
-#df paraules fènix
-ruta_json_fenix = 'llistes/paraules_fenixs.json' 
-
-with open(ruta_json_fenix, 'r', encoding='utf-8') as arxiu:
-    dades_fenix = json.load(arxiu)
-llista_paraules_fenix = [item['paraula'] for item in dades_fenix]
-
-df_rimes_fenix = df_rimes_totes[df_rimes_totes['Paraula'].isin(llista_paraules_fenix)].copy()
 
 #dades i coses del temps
 avui = datetime.now(tz_espanya).date()
@@ -50,12 +46,36 @@ ahir = avui - timedelta(days=1)
 data_inici = pd.to_datetime(inici_setmana)
 data_fi = pd.to_datetime(ahir) + timedelta(days=1, seconds=-1)
 
-mascara_temps_net = (df_net['Data'] >= data_inici) & (df_net['Data'] <= data_fi)
-df_net_setmana = df_net[mascara_temps_net].copy()
 
-mascara_temps_rimes = (df_rimes_totes['Data'] >= data_inici) & (df_rimes_totes['Data'] <= data_fi)
-df_rimes_setmana = df_rimes_totes[mascara_temps_rimes].copy()
+#df paraules fènix
+ruta_json_fenix = 'llistes/paraules_fenixs.json' 
 
+with open(ruta_json_fenix, 'r', encoding='utf-8') as arxiu:
+    dades_fenix = json.load(arxiu)
+llista_paraules_fenix = [item['paraula'] for item in dades_fenix]
+
+df_rimes_fenix = paraules_cercades_usuaris_diferents[paraules_cercades_usuaris_diferents['Paraula'].isin(llista_paraules_fenix)].copy()
+
+
+#les dues llistes
+df_typos = paraules_cercades_usuaris_diferents[paraules_cercades_usuaris_diferents['Rima'] == '***'].copy()
+
+
+mascara_cerques = (cerques_totals['Data'] >= data_inici) & (cerques_totals['Data'] <= data_fi)
+cerques_totals_emmascarat = cerques_totals[mascara_cerques].copy()
+
+paraules_cercades_emmascarades = cerques_totals_emmascarat.drop_duplicates(subset=['Paraula']).copy()
+paraules_cercades_emmascarades = paraules_cercades_emmascarades[paraules_cercades_emmascarades['Rima'] != '***'].copy()
+
+paraules_cercades_usuaris_diferents_emmascarat = cerques_totals_emmascarat.drop_duplicates(subset=['Paraula', 'Usuari']).copy()
+
+rimes_usuaris_diferents_emmascarat = cerques_totals_emmascarat.drop_duplicates(subset=['Paraula', 'Tipus de rima', 'Usuari']).copy()
+rimes_usuaris_diferents_emmascarat = rimes_usuaris_diferents_emmascarat[['Rima', 'Tipus de rima']]
+rimes_usuaris_diferents_emmascarat = rimes_usuaris_diferents_emmascarat[rimes_usuaris_diferents_emmascarat['Rima'] != '***'].copy()
+
+noms_propis = paraules_cercades[paraules_cercades['Codi'].str.startswith('NP')].copy()
+
+print(noms_propis.head())
 
 #funcions
 def obtenir_top_paraules(df_dades, n, paraula):
@@ -137,32 +157,34 @@ def formatar_top_per_json(dades):
 dades_json = {
     "actualitzacio": datetime.now(tz_espanya).strftime("%d/%m/%Y %H:%M:%S"),
     "setmana": {
-        "text_dies": f"{inici_setmana.day}/{inici_setmana.month} > {ahir.day}/{ahir.month}",        
-        "top_10_paraules": formatar_top_per_json(obtenir_top_paraules(df_rimes_setmana, 10, 'paraula')),
-        "top_10_rimes": formatar_top_per_json(obtenir_top_paraules(df_rimes_setmana, 10, 'rima')),
-        "total_cerques": len(df_net_setmana),
-        "numero_usuaris": df_net_setmana['Usuari'].nunique(),
-        "recompte_tipus_rima": df_net_setmana['Tipus de rima'].value_counts().to_dict(),
+        "text_dies": f"{inici_setmana.day}/{inici_setmana.month} > {ahir.day}/{ahir.month}",
+        "top_10_paraules": formatar_top_per_json(obtenir_top_paraules(paraules_cercades_usuaris_diferents_emmascarat, 10, 'paraula')),
+        "top_10_rimes": formatar_top_per_json(obtenir_top_paraules(rimes_usuaris_diferents_emmascarat, 10, 'rima')),
+        "total_cerques": len(cerques_totals_emmascarat),
+        "paraules_cercades_uniques": len(paraules_cercades_emmascarades),
+        "numero_usuaris": cerques_totals_emmascarat['Usuari'].nunique(),
 
     },
     "sempre": {
-        "top_10_paraules": formatar_top_per_json(obtenir_top_paraules(df_rimes_totes, 10, 'paraula')),
-        "top_10_rimes": formatar_top_per_json(obtenir_top_paraules(df_rimes_totes, 10, 'rima')),
-        "total_cerques": len(df_net),
-        "numero_usuaris": df_net['Usuari'].nunique(),
-        "recompte_tipus_rima": df_net['Tipus de rima'].value_counts().to_dict(),
+        "top_10_paraules": formatar_top_per_json(obtenir_top_paraules(paraules_cercades_usuaris_diferents, 10, 'paraula')),
+        "top_10_rimes": formatar_top_per_json(obtenir_top_paraules(rimes_usuaris_diferents, 10, 'rima')),
+        "total_cerques": len(cerques_totals),
+        "paraules_cercades_uniques": len(paraules_cercades),
+        "numero_usuaris": cerques_totals['Usuari'].nunique(),
+        "recompte_tipus_rima": cerques_totals['Tipus de rima'].value_counts().to_dict(),
         "top_10_fenix": formatar_top_per_json(obtenir_top_paraules(df_rimes_fenix, 10, 'paraula')),
         "top_10_typos": formatar_top_per_json(obtenir_top_paraules(df_typos, 10, 'paraula')),
-        "recompte_num_sil": df_filtres['Num. síl·'].value_counts().to_dict(),
-        "recompte_comenca_per": df_filtres['Comença per'].value_counts().to_dict(),
-        "recompte_incloure_np": df_filtres['Incloure NP'].value_counts().to_dict(),
-        "recompte_incloure_pl": df_filtres['Incloure pl.'].value_counts().to_dict(),
-        "top_dies": obtenir_top_dies(df_net)
+        "total_noms_propis": len(noms_propis),
+        "recompte_num_sil": cerques_totals['Num. síl·'].value_counts().to_dict(),
+        "recompte_comenca_per": cerques_totals['Comença per'].value_counts().to_dict(),
+        "recompte_incloure_np": cerques_totals['Incloure NP'].value_counts().to_dict(),
+        "recompte_incloure_pl": cerques_totals['Incloure pl.'].value_counts().to_dict(),
+        "top_dies": obtenir_top_dies(cerques_totals)
     },
     "grafics": {
-        "grafic_linia_diaria": dades_grafic_linia(df_net),
-        "grafic_formatge_exit_assonant": dades_grafic_formatge_totes(df_rimes_totes, "r.assonant"),
-        "grafic_formatge_exit_consonant": dades_grafic_formatge_totes(df_rimes_totes, "r.consonant"),
+        "grafic_linia_diaria": dades_grafic_linia(cerques_totals),
+        "grafic_formatge_exit_assonant": dades_grafic_formatge_totes(rimes_usuaris_diferents, "r.assonant"),
+        "grafic_formatge_exit_consonant": dades_grafic_formatge_totes(rimes_usuaris_diferents, "r.consonant"),
     }
 }
 
